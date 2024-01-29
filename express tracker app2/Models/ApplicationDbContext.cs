47b55be68544cef8fd67f078cf5e312fefb5ef88
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-29 15:27:11 +0100

    finally able to add migration to the database

---
+++ express tracker app2/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿//using statement
+using Microsoft.EntityFrameworkCore;
+
+namespace express_tracker_app2.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
---
+++ express tracker app2/Models/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace express_tracker_app2.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+        //Category id
+        //foregn key
+        public int CategoryId { get; set; }
+        public Category Catogory { get; set; }
+        public int Amount { get; set; }
+        [Column(TypeName = "nvarchar(75)")]
+        public string? Note { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+
+
+    }
+}
