2d16ff64121c208e39eb5ecb7a7da09da0ef1e54
Paul Richard <112706825+PaulRichardCode@users.noreply.github.com>
2024-01-29 14:45:13 +0100

    Add project files.

---
+++ express tracker app2/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace express_tracker_app2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
